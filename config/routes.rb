d458d074d647d041c1f663e2bb19075bfd3265b9
Reo05-code <t.asaishi05@gmail.com>
2025-10-24 19:59:49 +0900

    init commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get "posts/index"
+
+  get "up" => "rails/health#show", as: :rails_health_check
+
+
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+
+  root "posts#index"
+end
